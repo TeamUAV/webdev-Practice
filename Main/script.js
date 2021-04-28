d76ace7667d22c396430681da349e921f40ff894
Rajvi Jasani <rajvijasani1@gmail.com>
2021-04-29 01:44:19 +0530

    Added message bubble

---
+++ Main/script.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector('#send');
+const msgDisplay = document.querySelector('.msg-display');
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const msgBox = document.querySelector('#msgbox');
+    const text = msgBox.value;
+    msgBox.value = "";
+    const msgBubble = document.createElement('div');
+    msgBubble.classList.add('msg-bubble');
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.innerHTML = text;
+    const nameTag = document.createElement('div');
+    nameTag.classList.add('name-tag');
+    nameTag.innerHTML = "rajvi";
+    msgBubble.appendChild(content);
+    msgBubble.appendChild(nameTag);
+    msgDisplay.appendChild(msgBubble);
+});
